959da9498200a8fa00223d8a016cde35ba9ce5db
it22366122 <it22366122@my.sliit.lk>
2024-05-12 10:41:41 +0530

    initialize View model

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -6,13 +6,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.taskmanager.database.TaskDatabase
+import com.example.taskmanager.repo.taskRepo
+import com.example.taskmanager.viewmodel.TaskViewModel
+import com.example.taskmanager.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+        setupViewModel()
 
     }
+     private fun  setupViewModel(){
+         val taskRepo = taskRepo(TaskDatabase(this))
+         val viewModelProviderFactory = TaskViewModelFactory(application,taskRepo)
+         taskViewModel = ViewModelProvider(this,viewModelProviderFactory)[TaskViewModel::class.java]
+
+     }
+
 }
